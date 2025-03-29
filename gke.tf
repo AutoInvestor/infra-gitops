3d2141c8b9bbd1d02e8f600311d115ec8c4de1a6
Álvaro Mañoso Oca <43934777+alvaromaoc@users.noreply.github.com>
2025-03-29 20:40:44 +0100

    AIR-15 e2-medium (#23)

--- gke.tf
+++ gke.tf
@@ -13,7 +13,7 @@ resource "google_container_node_pool" "primary_default_pool" {
   node_count = 2
 
   node_config {
-    machine_type = "e2-micro"
+    machine_type = "e2-medium"
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
