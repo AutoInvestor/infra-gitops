ba27a18bd90f268ce69ad1ffc1dbc0516e575893
alvaromaoc <alvaromaoc@gmail.com>
2025-04-20 15:07:37 +0200

    Workload identity

--- gke.tf
+++ gke.tf
@@ -1,9 +1,52 @@
+locals {
+  gke_nodes_sa_roles = [
+    "roles/logging.logWriter",
+    "roles/monitoring.metricWriter",
+    "roles/pubsub.subscriber",
+    "roles/pubsub.publisher",
+  ]
+}
+
+resource "google_service_account" "gke_nodes_sa" {
+  account_id   = "gke-nodes-service-account"
+  display_name = "GKE Nodes Service Account"
+}
+
+resource "google_project_iam_member" "gke_nodes_sa_role" {
+  for_each = toset(local.gke_nodes_sa_roles)
+
+  project = var.project_id
+  role    = each.key
+  member  = "serviceAccount:${google_service_account.gke_nodes_sa.email}"
+}
+
+resource "kubernetes_service_account" "gke_nodes_ksa" {
+  metadata {
+    name      = "gke-nodes-kubernetes-service-account"
+    namespace = "autoinvestor"
+  }
+}
+
 resource "google_container_cluster" "primary" {
   name     = var.gke_cluster_name
   location = var.gke_zone
 
+  workload_identity_config {
+    workload_pool = "${var.project_id}.svc.id.goog"
+  }
+
   remove_default_node_pool = true
   initial_node_count       = 1
+}
+
+resource "google_service_account_iam_binding" "gke_nodes_sa_ksa_binding" {
+  service_account_id = google_service_account.gke_nodes_sa.id
+
+  role = "roles/iam.workloadIdentityUser"
+
+  members = [
+    "serviceAccount:${google_container_cluster.primary.workload_identity_config[0].workload_pool}.svc.id.goog[${kubernetes_service_account.gke_nodes_ksa.metadata[0].namespace}/${kubernetes_service_account.gke_nodes_ksa.metadata[0].name}]"
+  ]
 }
 
 resource "google_container_node_pool" "primary_default_pool" {
@@ -14,6 +57,8 @@ resource "google_container_node_pool" "primary_default_pool" {
 
   node_config {
     machine_type = "e2-small"
+
+    service_account = google_service_account.gke_nodes_sa.email
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
