4f8d0f671a3da8d3543f6e81f364a8bb2a6cfa20
Álvaro Mañoso Oca <43934777+alvaromanoso@users.noreply.github.com>
2025-04-21 12:23:50 +0200

    Workload identity fix (#35)

--- gke.tf
+++ gke.tf
@@ -64,3 +64,13 @@ resource "google_container_node_pool" "primary_default_pool" {
     ]
   }
 }
+
+data "google_client_config" "provider" {}
+
+provider "kubernetes" {
+  host  = "https://${google_container_cluster.primary.endpoint}"
+  token = data.google_client_config.provider.access_token
+  cluster_ca_certificate = base64decode(
+    google_container_cluster.primary.master_auth[0].cluster_ca_certificate,
+  )
+}
