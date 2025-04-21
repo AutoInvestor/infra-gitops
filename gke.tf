37217e327062aff68d53213eceeb603051809c35
alvaromaoc <alvaromaoc@gmail.com>
2025-04-21 16:58:50 +0200

    e2-medium

--- gke.tf
+++ gke.tf
@@ -62,7 +62,7 @@ resource "google_container_node_pool" "primary_default_pool" {
   node_count = 2
 
   node_config {
-    machine_type = "e2-small"
+    machine_type = "e2-medium"
 
     service_account = google_service_account.gke_nodes_sa.email
     oauth_scopes = [
