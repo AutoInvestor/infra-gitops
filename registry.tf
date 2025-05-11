48caec5739ccc48f95f3a828764c18e44857a3a1
alvaromaoc <alvaromaoc@gmail.com>
2025-05-11 20:52:20 +0200

    Docker repo

--- registry.tf
+++ registry.tf
@@ -7,3 +7,13 @@ resource "google_artifact_registry_repository" "helm_repo" {
   format             = "DOCKER"
   mode               = "STANDARD_REPOSITORY"
 }
+
+resource "google_artifact_registry_repository" "docker_repo" {
+  depends_on = [google_project_service.active_api]
+
+  location           = data.google_client_config.provider.region
+  repository_id      = "docker-images"
+  description        = "Docker images repository"
+  format             = "DOCKER"
+  mode               = "STANDARD_REPOSITORY"
+}
